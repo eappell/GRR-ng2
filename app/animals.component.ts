0b918fd5bd6151aebde587a27f5e4fb642a87006
Appell. Eddie <AppellE@saccounty.net>
2016-08-30 14:57:58 -0700

    Added animals component

---
+++ app/animals.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { http } from '@angular/http';
+
+@Component({
+    selector: 'animals',
+    template: '<h3>Collection</h3>'
+})
+export class AnimalsComponent { }
