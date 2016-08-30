a17377042b144d2a22d2b26e754158f6947aa60d
Appell. Eddie <AppellE@saccounty.net>
2016-08-30 15:15:12 -0700

    updates

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AnimalsComponent } from './animals.component';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, AnimalsComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
